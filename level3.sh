0c7db08ac42e77e18d35c234be05437a919ea4db
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2025-05-29 00:08:02 +0100

    use simpler solution for level3

--- level3.sh
+++ level3.sh
@@ -4,12 +4,8 @@
 # Arena directory. If it does, print Hero found!; otherwise,
 # print Hero missing!.
 
-files=$(ls -l ./Arena | awk '{print $9}' | grep "hero.txt")
-## there is probably a better way to do this haha
-
-if [ "$files" == "hero.txt" ]
-then
-	echo "Hero found!"
+if [ -f "Arena/hero.txt" ]; then
+    echo "Hero found!"
 else
-	echo "Hero missing!"
+    echo "Hero missing!"
 fi
