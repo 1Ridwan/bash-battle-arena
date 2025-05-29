4996c34e706c278b3d8f4768d10fb53122ff612d
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2025-05-29 23:57:37 +0100

    making level6 solution also display data size of file

--- level6.sh
+++ level6.sh
@@ -6,14 +6,18 @@ Mission: Write a script that accepts a filename as an argument and
  display a message saying 'No file provided'.
 '
 
+if [ -z "$1" ];
+then
+    echo "No file provided"
+    exit 1
+fi
 
-print_lines() {
-    if [[ -f "./$1" ]];
-    then
-        awk 'END { print NR }' ./$1
-    else    
-        echo "File not found: ./$1"
-    fi
-}
+if [ ! -f "$1" ];
+then
+    echo "file not found"
+    exit 1
+fi
 
-print_lines "hero.txt"
+LINE_COUNT=$( wc -l < "$1" )
+DATA_COUNT=$( wc -c < "$1" )
+echo "The file $1 has"$LINE_COUNT" lines and"$DATA_COUNT" bytes of data."
